be4cb7d7c27106ab6ef3c760d72a6679ebe17365
dddCrazy87 <n126970@gmail.com>
2024-06-05 23:27:03 +0800

    update end

--- DTD Future/Scenes/bad_end.swift
+++ DTD Future/Scenes/bad_end.swift
@@ -13,11 +13,21 @@ struct bad_end: View {
             Text("變成知名便利商店刷條碼的員工，\n下班時間摸魚還被老闆抓包，\n是數位系的恥辱！")
                 .font(.largeTitle)
             
-            Button {
-                updateViewController(GlobalState.shared, view: "StartView", video: "none")
-            } label: {
-                Text("重新開始")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            HStack {
+                
+                Button {
+                    updateViewController(GlobalState.shared, view: "StartView", video: "none")
+                } label: {
+                    Text("重新開始")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+                
+                Button {
+                    updateViewController(GlobalState.shared, view: "Floor1_car_game", video: "none")
+                } label: {
+                    Text("試試別的結局")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
             }
         }
     }
--- DTD Future/Scenes/good_end.swift
+++ DTD Future/Scenes/good_end.swift
@@ -13,11 +13,21 @@ struct good_end: View {
             Text("你成為了知名遊戲公司的遊戲設計師，\n身價破億，\n是數位系的驕傲！")
                 .font(.largeTitle)
             
-            Button {
-                updateViewController(GlobalState.shared, view: "StartView", video: "none")
-            } label: {
-                Text("重新開始")
-                    .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            HStack {
+                
+                Button {
+                    updateViewController(GlobalState.shared, view: "StartView", video: "none")
+                } label: {
+                    Text("重新開始")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
+                
+                Button {
+                    updateViewController(GlobalState.shared, view: "Floor1_car_game", video: "none")
+                } label: {
+                    Text("試試別的結局")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                }
             }
         }
     }
